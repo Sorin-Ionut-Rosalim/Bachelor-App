4e60bfd811be9db8e3643bb7bc984d0431a933d3
srosalim <rosalim.sorinionut@gmail.com>
2023-03-20 00:17:30 +0200

    Added buttons, LibraryActivity and more. Not working properly. Need to find a good qrscanner lib.

--- app/src/main/java/com/example/arapp/MainActivity.java
+++ app/src/main/java/com/example/arapp/MainActivity.java
@@ -1,9 +1,16 @@
 package com.example.arapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 
 import com.google.ar.core.Anchor;
 import com.google.ar.sceneform.AnchorNode;
@@ -14,21 +21,55 @@ import com.google.ar.sceneform.ux.TransformableNode;
 public class MainActivity extends AppCompatActivity {
 
     private ArFragment arFragment;
-
-//    Button mainButton = findViewById(R.id.main_button);
-//    mainButton.setOnClickListener(new View.OnClickListener() {
-//        @Override
-//        public void onClick(View view) {
-//            // Show the three buttons when the main button is clicked
-//        }
-//    });
+    private  Button burgerButton, qrButton, libraryButton;
+    private LinearLayout hiddenButtons;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        arFragment = (ArFragment)getSupportFragmentManager().findFragmentById(R.id.arFragment);
+        burgerButton = findViewById(R.id.burgerButton);
+        qrButton = findViewById(R.id.qrButton);
+        libraryButton = findViewById(R.id.libraryButton);
+        hiddenButtons = findViewById(R.id.button_holder);
+
+        burgerButton.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View view) {
+              if (hiddenButtons.getVisibility() == View.GONE) {
+                  hiddenButtons.setVisibility(View.VISIBLE);
+                  burgerButton.setText("Hide");
+              }
+              else
+              {
+                  hiddenButtons.setVisibility(View.GONE);
+                  burgerButton.setText("Show");
+              }
+          }
+        });
+
+        libraryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, LibraryActivity.class);
+                startActivity(intent);
+            }
+        });
+
+//        qrButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                FragmentManager fragmentManager = getSupportFragmentManager();
+//                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+//                QRscannerFragment qRscannerFragment = new QRscannerFragment();
+//                fragmentTransaction.replace(R.id.arFragment, qRscannerFragment);
+//                fragmentTransaction.addToBackStack(null);
+//                fragmentTransaction.commit();
+//            }
+//        });
+
+        arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.arFragment);
 
         assert arFragment != null;
         arFragment.setOnTapArPlaneListener(((hitResult, plane, motionEvent) -> {
---
+++ app/src/main/java/com/example/arapp/ScannerActivity.java
@@ -0,0 +1,10 @@
+package com.example.arapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ScannerActivity extends AppCompatActivity {
+
+
+}
