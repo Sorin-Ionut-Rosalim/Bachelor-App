2dd80effb67b2027fc1b68233978768eefced50e
Ionut Rosalim <rosalim.sorinionut@gmail.com>
2023-05-09 16:49:09 +0300

    Start of library menu

--- app/src/main/java/com/alpha/RealityEnhance/LibraryActivity.java
+++ app/src/main/java/com/alpha/RealityEnhance/LibraryActivity.java
@@ -5,11 +5,18 @@ import static android.content.Intent.FLAG_ACTIVITY_NO_ANIMATION;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LibraryActivity extends AppCompatActivity {
+
+    Button btn;
+    Button newBtn;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -19,6 +26,7 @@ public class LibraryActivity extends AppCompatActivity {
 
         ImageView qrButton = findViewById(R.id.qrButton);
         ImageView backButton = findViewById(R.id.backButton);
+
 
         qrButton.setOnClickListener(view -> {
             Intent intent = new Intent(LibraryActivity.this, QRActivity.class);
@@ -31,6 +39,21 @@ public class LibraryActivity extends AppCompatActivity {
             intent.addFlags(FLAG_ACTIVITY_NO_ANIMATION);
             startActivity(intent);
         });
+
+        btn = findViewById(R.id.button);
+
+        btn.setOnClickListener(view -> {
+            addButton();
+        });
+    }
+
+    private void addButton() {
+        String path = Environment.getExternalStorageDirectory().toString() + "ass";
+        Toast.makeText(LibraryActivity.this, path, Toast.LENGTH_SHORT).show();
+        LinearLayout layout = findViewById(R.id.linear_Layout);
+        newBtn = new Button(this);
+        newBtn.setText("New Button");
+        layout.addView(newBtn);
     }
 
 }
